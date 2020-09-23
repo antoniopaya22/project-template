f595a05a54598f81058727d38c48453a15511840
Antonio Paya <antonioalfa22@gmail.com>
2020-09-23 08:56:12 +0200

    Added prometheus exporter to backend

--- backend/Startup.cs
+++ backend/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Prometheus;
 
 namespace ASP.NETCore_API_REST_Template
 {
@@ -51,16 +52,24 @@ namespace ASP.NETCore_API_REST_Template
                 context.Database.EnsureCreated();
             }
 
+            // Custom Metrics to count requests for each endpoint and the method
+            var counter = Metrics.CreateCounter("peopleapi_path_counter", "Counts requests to the People API endpoints", new CounterConfiguration
+            {
+                LabelNames = new[] { "method", "endpoint" }
+            });
+            app.Use((context, next) =>
+            {
+                counter.WithLabels(context.Request.Method, context.Request.Path).Inc();
+                return next();
+            });
+
+            // Middlewares
+            app.UseMetricServer();
             app.UseCors("CorsPolicy");
-
             app.UseRouting();
-
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
         }
     }
 }
